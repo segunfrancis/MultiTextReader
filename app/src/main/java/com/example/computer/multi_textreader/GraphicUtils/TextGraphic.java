6a734eb2b064a6748f63924e4c2cdd17657b2a5d
segunfrancis <francis4dblues@gmail.com>
2019-04-09 20:19:26 +0100

    Add GraphicOverlay class

---
+++ app/src/main/java/com/example/computer/multi_textreader/GraphicUtils/TextGraphic.java
@@ -0,0 +1,12 @@
+package com.example.computer.multi_textreader.GraphicUtils;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.hardware.camera2.CameraCharacteristics;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TextGraphic
