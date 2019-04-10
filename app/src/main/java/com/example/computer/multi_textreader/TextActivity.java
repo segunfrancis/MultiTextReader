f65277a46910b7cf505b1eab66f48e8e7eb7b7f9
segunfrancis <francis4dblues@gmail.com>
2019-04-10 16:36:27 +0100

    Add TextToSpeech functionality

--- app/src/main/java/com/example/computer/multi_textreader/TextActivity.java
+++ app/src/main/java/com/example/computer/multi_textreader/TextActivity.java
@@ -1,9 +1,14 @@
 package com.example.computer.multi_textreader;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +18,13 @@ public class TextActivity extends AppCompatActivity {
     @BindView(R.id.display_text)
     TextView displayText;
 
+    @BindView(R.id.play_button)
+    Button playButton;
+
+    private TextToSpeech toSpeech;
+
+    private String gottenText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +32,31 @@ public class TextActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        String gottenText = intent.getStringExtra("text");
+        gottenText = intent.getStringExtra("text");
         displayText.setText(gottenText);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readText();
+            }
+        });
+    }
+
+    private void readText() {
+        toSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                toSpeech.setLanguage(Locale.UK);
+                toSpeech.speak(gottenText, TextToSpeech.QUEUE_ADD, null);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        toSpeech.shutdown();
     }
 }
